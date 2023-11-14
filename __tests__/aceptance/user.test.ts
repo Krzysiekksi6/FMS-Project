e07d124b2b3c62cfbf3038c1f3232d07d0dd900b
Krzysztof <krzysiekksi6@gmail.com>
2023-11-14 20:01:43 +0100

    fix/repair bug with no reflect metadata in tests files, now dataSource is imported from main config file connectDatabase.ts

--- __tests__/aceptance/user.test.ts
+++ __tests__/aceptance/user.test.ts
@@ -1,17 +1,22 @@
-import {createConnection} from "typeorm";
+import {createConnection, DataSource} from "typeorm";
+import * as request  from "supertest";
 import app from "../../src/app";
 import config from "../../src/config"
+import {connectDatabase} from "../../src/config/connectDatabase";
 
-let connection, server
-beforeEach(async () => {
-    connection = await createConnection()
-    await  connection.synchronize()
-    server = app.listen(config.port)
-})
-afterEach(() => {
-    connection.close();
+let  myDataSource = connectDatabase;
+let server;
+beforeAll(async() => {
+    await myDataSource.connect()
+    server = app.listen(config.port);
+});
+
+afterAll(() => {
     server.close();
-})
-it('simple test', () => {
-    expect(1 + 1).toBe(2)
+    myDataSource.close()
+});
+it('Should be no users initially', async() => {
+    const response = await request(app).get('/users')
+    console.log(response.body)
+    expect(response.statusCode).toBe(200);
 })
